edd196056e763b8ab242212ffe4bc3a09b23b629
ryder <ryderortiz7@gmail.com>
2025-06-28 13:54:07 -0400

    initialized python program file, store coordinates

---
+++ main_program.py
@@ -0,0 +1,13 @@
+# Store coordinates for each section in the store
+store_coords = {
+    "Entrance Left": (0, 0),
+    "Entrance Right": (20, 0),
+    "Pharmacy": (6, 1),
+    "Books": (9, 6),
+    "Deli": (19, 11),
+    "Frozen": (18, 9),
+    "Electronics": (9, 12),
+    "Kitchen & Dining": (4, 7),
+    "Personal Care & Beauty": (3, 2),
+    # add more sections as needed
+}
